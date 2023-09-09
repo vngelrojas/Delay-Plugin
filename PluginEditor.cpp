42eabfb61c3cd15e1e8db965cb2e7445ad969a5f
Angel Rojas <aarojas@usc.edu>
2023-09-08 20:22:25 -0700

    Added slider

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -16,6 +16,16 @@ DelayPluginAudioProcessorEditor::DelayPluginAudioProcessorEditor (DelayPluginAud
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    waveZoom.setRange(12000.0f, 48000.0f, 100.0f);
+    waveZoom.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    waveZoom.setTextBoxStyle(juce:: Slider::TextBoxBelow, true, 50, 20);
+    addAndMakeVisible(waveZoom);
+
+    waveZoom.onValueChange = [this]()
+    {
+        audioProcessor.delay.setDelay((float)waveZoom.getValue());
+    };
 }
 
 DelayPluginAudioProcessorEditor::~DelayPluginAudioProcessorEditor()
@@ -30,11 +40,12 @@ void DelayPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    
 }
 
 void DelayPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    waveZoom.setBounds(getLocalBounds().withSizeKeepingCentre(getWidth() * 0.5, getHeight() * 0.5));
 }
--- PluginEditor.h
+++ PluginEditor.h
@@ -29,5 +29,7 @@ private:
     // access the processor object that created it.
     DelayPluginAudioProcessor& audioProcessor;
 
+    juce::Slider waveZoom;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayPluginAudioProcessorEditor)
 };
