2361acff4346f2e44dc778e8dbffcc26b7adcde7
Angel Rojas <aarojas@usc.edu>
2023-09-08 22:51:12 -0700

    Added FourHeadDelay class files

---
+++ FourHeadDelay/FourHeadDelay.h
@@ -0,0 +1,13 @@
+/*
+  ==============================================================================
+
+    FourHeadDelay.h
+    Created: 8 Sep 2023 10:48:45pm
+    Author:  vngel
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+template <typename SampleType, typename InterpolationType = DelayLineInterpolationTypes::Linear>
