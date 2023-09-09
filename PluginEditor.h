c6b521033d9023fe5c97ae1adc780145bfc5d7e5
Angel Rojas <aarojas@usc.edu>
2023-09-08 20:35:15 -0700

    Added delay logic to proccessAudio

--- PluginEditor.h
+++ PluginEditor.h
@@ -28,7 +28,6 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     DelayPluginAudioProcessor& audioProcessor;
-
     juce::Slider waveZoom;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayPluginAudioProcessorEditor)
